cbcdad4eb2225e503866b462f45351aaa5c3daf4
Your Name <you@example.com>
2022-03-01 21:01:09 +0100

    add express nodemon dotenv mongoose done!

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+require("dotenv").config({ path: "./config/.env" });
+require("./config/db");
+
+const app = express();
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
