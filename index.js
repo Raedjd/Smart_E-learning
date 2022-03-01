45df035dc842d8f46963305d051f2f68d48f0500
Your Name <you@example.com>
2022-03-01 21:06:17 +0100

    start!

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
 
 const app = express();
+
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });
