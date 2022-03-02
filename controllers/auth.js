1dda600a9ce252c1c98006c95e511eda137ca284
Your Name <you@example.com>
2022-03-02 20:27:33 +0100

    add @sendgrid/mail done!

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const UserModel = require("../models/user");
-
+const sgMail = require("@sendgrid/mail");
 //register user
 module.exports.signUp = async (req, res) => {
   const { username, email, password } = req.body;
