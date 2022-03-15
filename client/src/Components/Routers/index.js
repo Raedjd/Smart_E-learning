a9854ea76456dff1d2d7dcb8a1677e537df9b0f7
Your Name <you@example.com>
2022-03-15 09:37:38 +0100

    update!

--- client/src/Components/Routers/index.js
+++ client/src/Components/Routers/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "../Pages/Header";
 import Home from "../Pages/Home";
-import SignUp from "../Pages/SignUp";
-import SignIn from "../Pages/SignIn";
-import ForgetPassword from "../Pages/ForgetPassword";
-import ResetPassword from "../Pages/ResetPassword";
+import SignUp from "../Pages/User/SignUp";
+import SignIn from "../Pages/User/SignIn";
+import ForgetPassword from "../Pages/User/ForgetPassword";
+import ResetPassword from "../Pages/User/ResetPassword";
 const index = () => {
   return (
     <BrowserRouter>
