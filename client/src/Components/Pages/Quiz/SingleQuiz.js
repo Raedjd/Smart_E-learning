492a0febd94e9dec45aa6e5db8c12bc7b239f118
mohamed lahbibi <lahbibi.mohamed@esprit.tn>
2022-04-26 13:15:34 +0100

    quiz card css fix

--- client/src/Components/Pages/Quiz/SingleQuiz.js
+++ client/src/Components/Pages/Quiz/SingleQuiz.js
@@ -19,42 +19,61 @@ function SingleQuiz({ quiz }) {
     quizes.delete(`/delete-quiz/${quiz._id}`);
   };
   return (
-    <div className="col-lg-5 col-md-6 card-quiz">
-      <div className="cardFeature">
-        <h5 className="font-title--xs text-primary"> {quiz.title}</h5>
-        <h5 className="font-title--xs">{`level :  ${quiz.level}`}</h5>
-        <h5> {`date of  creation :  ${dateParser(quiz.createdAt)}`}</h5>
 
-        <hr></hr>
-        <div className="singlequiz-btn">
-          <Link
-            hidden={userData.role === "teacher" || userId === undefined}
-            className="btn btn-outline-success singlequiz-btn-detail"
-            to={`/pass-quiz/${quiz._id}`}
-          >
-            {" "}
-            Pass quiz
-          </Link>
-          <Link
-            className="btn btn-outline-primary singlequiz-btn-detail"
-            to={`/quiz/${quiz._id}`}
-          >
-            {" "}
-            more details
-          </Link>
-          <button
-            hidden={
-              !(window.location.href === "http://localhost:3000/profilstudent")
-            }
-            onClick={deletequizHandler}
-            className="btn btn-outline-danger singlequiz-btn"
-          >
-            delete
-          </button>
-        </div>
+    <div className="col-lg-5 col-md-6 card-quiz ">
+
+    <div className="cardFeature">
+    <div className={`ribbon quiz-${quiz.level}`}><span>{quiz.level}</span></div>
+
+      <h5 className="font-title--xs text-primary "> {quiz.title}</h5>
+      <h5> {`date of  creation :  ${dateParser(quiz.createdAt)}`}</h5>
+
+
+      <hr></hr>
+      <div className="singlequiz-btn">
+        <Link
+          hidden={userData.role === "teacher" || userId === undefined}
+          className="btn btn-outline-success singlequiz-btn-detail"
+          to={`/pass-quiz/${quiz._id}`}
+        >
+          {" "}
+          Pass quiz
+        </Link>
+        <Link
+          className="btn btn-outline-primary singlequiz-btn-detail"
+          to={`/quiz/${quiz._id}`}
+        >
+          {" "}
+          more details
+        </Link>
+        <button
+          hidden={
+            !(window.location.href === "http://localhost:3000/profilstudent")
+          }
+          onClick={deletequizHandler}
+          className="btn btn-outline-danger singlequiz-btn"
+        >
+          delete
+        </button>
       </div>
     </div>
+  </div>
   );
 }
 
 export default SingleQuiz;
+/*
+
+*/
+/*
+    <div>
+          <div class="box">
+  <div class="ribbon"><span>New</span></div>
+</div>
+<div class="box">
+  <div class="ribbon rouge"><span>Popular</span></div>
+</div>
+<div class="box">
+  <div class="ribbon bleu"><span>Popular</span></div>
+</div>
+    </div>*/
